2b389a19fc8a95a1ccd7d2b92b2d47b022aa7f09
Bluscream <Bluscream@users.noreply.github.com>
2024-02-01 05:29:12 +0100

    Refactor stats.py to improve readability and add MQTT integration

--- responses/data.py
+++ responses/data.py
@@ -1,89 +1,135 @@
-from typing import List
-from typing import Any
-from dataclasses import dataclass
-import json
-@dataclass
-class Data:
-    isConnected: bool
-    negotiatedValues: List[NegotiatedValue]
-    errorCounters: List[ErrorCounter]
-    dataType: str
+from typing import Optional, List, Union, Any
 
-    @staticmethod
-    def from_dict(obj: Any) -> 'Data':
-        _isConnected = 
-        _negotiatedValues = [NegotiatedValue.from_dict(y) for y in obj.get("negotiatedValues")]
-        _errorCounters = [ErrorCounter.from_dict(y) for y in obj.get("errorCounters")]
-        _dataType = str(obj.get("dataType"))
-        return Data(_isConnected, _negotiatedValues, _errorCounters, _dataType)
+class ValClass:
+    us: Optional[str]
+    ds: str
 
-@dataclass
+    def __init__(self, us: Optional[str], ds: str) -> None:
+        self.us = us
+        self.ds = ds
+
+    @classmethod
+    def from_dict(cls, json: dict):
+        us = json['us'] if 'us' in json else None
+        ds = json['ds']
+        return cls(us, ds)
+
 class ErrorCounter:
     title: str
-    val: List[Val]
+    val: Optional[List[ValClass]]
 
-    @staticmethod
-    def from_dict(obj: Any) -> 'ErrorCounter':
-        _title = str(obj.get("title"))
-        _val = [Val.from_dict(y) for y in obj.get("val")]
-        return ErrorCounter(_title, _val)
+    def __init__(self, title: str, val: Optional[List[ValClass]]) -> None:
+        self.title = title
+        self.val = val
 
-@dataclass
+    @classmethod
+    def from_dict(cls, json: dict):
+        title = json['title']
+        val = [ValClass.from_dict(v) for v in json['val']] if 'val' in json else []
+        return cls(title, val)
+
+class NegotiatedValue:
+    unit: Optional[str]
+    title: str
+    val: Optional[List[Union[ValClass, str]]]
+
+    def __init__(self, unit: Optional[str], title: str, val: Optional[List[Union[ValClass, str]]]) -> None:
+        self.unit = unit
+        self.title = title
+        self.val = val
+
+    @classmethod
+    def from_dict(cls, json: dict):
+        unit = json['unit'] if 'unit' in json else None
+        title = json['title']
+        val = [ValClass.from_dict(v) if isinstance(v, dict) else v for v in json['val']] if 'val' in json else []
+        return cls(unit, title, val)
+
+class Data:
+    is_connected: bool
+    negotiated_values: List[NegotiatedValue]
+    error_counters: List[ErrorCounter]
+    data_type: str
+
+    def __init__(self, is_connected: bool, negotiated_values: List[NegotiatedValue], error_counters: List[ErrorCounter], data_type: str) -> None:
+        self.is_connected = is_connected
+        self.negotiated_values = negotiated_values
+        self.error_counters = error_counters
+        self.data_type = data_type
+
+    def get_all_values(self) -> dict[str, str]:
+        vals = {}
+        for val in self.negotiated_values:
+            if (len(val.val) > 0): vals[val.title] = val.val[0].ds if hasattr(val.val[0], "ds") else val.val[0]
+        return vals
+
+    def get_values(self, names: list[str]) -> dict[str, str]:
+        return {name: self.get_value(name) for name in names}
+
+    def get_value(self, name: str) -> Optional[str]:
+        return self.get_negotiated_value(name)[0].ds
+
+    def get_negotiated_value(self, name: str) -> Optional[List[ValClass]]:
+        for val in self.negotiated_values:
+            if val.title == name:
+                return val.val
+        return None
+    
+    def get_error_counter(self, name: str) -> Optional[List[ValClass]]:
+        for val in self.error_counters:
+            if val.title == name:
+                return val.val
+        return None
+
+    @classmethod
+    def from_dict(cls, json: dict):
+        is_connected = json['isConnected']
+        negotiated_values = [NegotiatedValue.from_dict(n) for n in json['negotiatedValues']]
+        error_counters = [ErrorCounter.from_dict(e) for e in json['errorCounters']]
+        data_type = json['type'] if 'type' in json else None
+        return cls(is_connected, negotiated_values, error_counters, data_type)
+
+
 class Hide:
-    provServ: bool
-    ssoSet: bool
+    prov_serv: bool
+    sso_set: bool
     rrd: bool
 
-    @staticmethod
-    def from_dict(obj: Any) -> 'Hide':
-        _provServ = 
-        _ssoSet = 
-        _rrd = 
-        return Hide(_provServ, _ssoSet, _rrd)
+    def __init__(self, prov_serv: bool, sso_set: bool, rrd: bool) -> None:
+        self.prov_serv = prov_serv
+        self.sso_set = sso_set
+        self.rrd = rrd
 
-@dataclass
-class NegotiatedValue:
-    unit: str
-    title: str
-    val: List[object]
+    @classmethod
+    def from_dict(cls, json: dict):
+        prov_serv = json['provServ']
+        sso_set = json['ssoSet']
+        rrd = json['rrd']
+        return cls(prov_serv, sso_set, rrd)
 
-    @staticmethod
-    def from_dict(obj: Any) -> 'NegotiatedValue':
-        _unit = str(obj.get("unit"))
-        _title = str(obj.get("title"))
-        _val = [.from_dict(y) for y in obj.get("val")]
-        return NegotiatedValue(_unit, _title, _val)
 
-@dataclass
 class Root:
     pid: str
     hide: Hide
-    timeTillLogout: str
-    time: List[object]
+    time_till_logout: int
+    time: List[Any]
     data: Data
     sid: str
 
-    @staticmethod
-    def from_dict(obj: Any) -> 'Root':
-        _pid = str(obj.get("pid"))
-        _hide = Hide.from_dict(obj.get("hide"))
-        _timeTillLogout = str(obj.get("timeTillLogout"))
-        _time = [.from_dict(y) for y in obj.get("time")]
-        _data = Data.from_dict(obj.get("data"))
-        _sid = str(obj.get("sid"))
-        return Root(_pid, _hide, _timeTillLogout, _time, _data, _sid)
+    def __init__(self, pid: str, hide: Hide, time_till_logout: int, time: List[Any], data: Data, sid: str) -> None:
+        self.pid = pid
+        self.hide = hide
+        self.time_till_logout = time_till_logout
+        self.time = time
+        self.data = data
+        self.sid = sid
 
-@dataclass
-class Val:
-    us: str
-    ds: str
-
-    @staticmethod
-    def from_dict(obj: Any) -> 'Val':
-        _us = str(obj.get("us"))
-        _ds = str(obj.get("ds"))
-        return Val(_us, _ds)
-
-# Example Usage
-# jsonstring = json.loads(myjsonstring)
-# root = Root.from_dict(jsonstring)
+    @classmethod
+    def from_dict(cls, json: dict):
+        pid = json['pid']
+        hide = Hide.from_dict(json['hide'])
+        time_till_logout = json['timeTillLogout']
+        time = json['time']
+        data = Data.from_dict(json['data'])
+        sid = json['sid']
+        return cls(pid, hide, time_till_logout, time, data, sid)
--- stats.py
+++ stats.py
@@ -1,24 +1,119 @@
-import http.client
-from .responses.data import Root
+from datetime import datetime
+from json import loads as json_loads
+# from json import dumps as json_dumps
+from xml.etree import ElementTree
+from hashlib import md5
+from sys import argv
+from ssl import _create_unverified_context
+from os import getenv
 
-payload = "xhr=1&sid=d94b7663ac22e56c&lang=en-US&page=dslStat&xhrId=refresh&useajax=1&no_sidrenew=1"
+
+import paho.mqtt.client as mqtt
+from http.client import HTTPSConnection, HTTPConnection
+from asyncio import sleep as sleep_s # from time import sleep
+from asyncio import run
+from dotenv import load_dotenv
+
+from responses.data import Root
+
+# get MQTT_HOST from dotenv
+
+load_dotenv()
+sid = ''
+important_values = [ 'DSLAM-Datenrate Max.', 'DSLAM-Datenrate Min.', 'Leitungskapazität' ]
+payload = {
+    'xhr': '1',
+    'lang': 'en',
+    'page': 'dslStat',
+    'xhrId': 'refresh',
+    'useajax': '1',
+    'no_sidrenew': '1'
+}
 headers = {
     'Accept': "*/*",
     'Accept-Language': "en-US,en;q=0.9",
     'Cache-Control': "no-cache",
     'Connection': "keep-alive",
     'Content-Type': "application/x-www-form-urlencoded",
-    'Origin': "http://192.168.2.1",
+    'Origin': f"http://{getenv('FRITZBOX_IP')}",
     'Pragma': "no-cache",
-    'Referer': "http://192.168.2.1/",
-    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0",
-    'x-request-modified': "1"
+    'Referer': f"http://{getenv('FRITZBOX_IP')}/",
+    'User-Agent': "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36 OPR/106.0.0.0"
     }
 
-conn = http.client.HTTPConnection("192.168.2.1")
-conn.request("POST", "/data.lua", payload, headers)
+last_log = ''
+def log(msg: str, ts: datetime = None):
+    global last_log
+    if msg == last_log: return
+    last_log = msg
+    print(f"{ts or datetime.now()} - {msg}")
+def sanitize(name: str) -> str:
+    return name.replace(" ", "-").replace(".", "_")
+def dicts_are_different(a: dict, b: dict) -> bool:
+    if a is None or b is None: return True
+    if a.keys() != b.keys(): return True
+    for key in a.keys():
+        if a[key] != b[key]: return True
+    return False
 
-res = conn.getresponse()
-data = res.read()
+mqtt_client = mqtt.Client()
+mqtt_client.connect(getenv("MQTT_IP"), int(getenv("MQTT_PORT")), 60)
+mqtt_client.loop_start()
+mqtt_client.username_pw_set(getenv("MQTT_USERNAME"), getenv("MQTT_PASSWORD"))
+def publish_stats(vals: dict[str, str]):
+    for name, val in vals.items():
+        mqtt_client.publish(f'fritzbox_extended/{sanitize(name)}', val)
 
-print(data.decode("utf-8"))
+
+conn = HTTPSConnection(
+    getenv("FRITZBOX_IP"),
+    context = _create_unverified_context()
+)
+def get_fritzbox_sid():
+    conn.request("GET", "/login_sid.lua")
+    response = conn.getresponse()
+    challenge = ElementTree.fromstring(response.read()).findall(".//Challenge")[0].text
+    hash = md5("{}-{}".format(challenge, getenv("FRITZBOX_PASSWORD")).encode("UTF-16LE")).hexdigest()
+    response_string = "{}-{}".format(challenge, hash)
+    conn.request("GET", "/login_sid.lua?username={}&response={}".format(getenv("FRITZBOX_USERNAME"), response_string))
+    response = conn.getresponse()
+    sid = ElementTree.fromstring(response.read()).findall(".//SID")[0].text
+    # conn.close()
+    log(f"Got new sid: {sid}")
+    return sid
+
+def get_stats():
+    global sid
+    payload['sid'] = sid if sid else get_fritzbox_sid()
+    _payload = "&".join([f"{key}={value}" for key, value in payload.items()])
+    conn.request("POST", "/data.lua", _payload, headers)
+    res = conn.getresponse()
+    if res.code == 303:
+        sid = get_fritzbox_sid()
+        return get_stats()
+    txt = res.read()
+    utf8 = txt.decode("utf-8")
+    try: _json = json_loads(utf8)
+    except Exception as e: raise Exception(f"Error parsing json: {utf8}") from e
+    _class = Root.from_dict(_json)
+    return _class
+
+async def main():
+    last_vals = None
+    while True:
+        try:
+            stats = get_stats()
+            vals = stats.data.get_all_values() # .get_values(important_values)
+            if not dicts_are_different(vals, last_vals):
+                log("No change")
+                continue
+            # for name, val in vals.items():
+            #     log(f"{name}: {val}")
+            log(f"DSLAM: {vals['DSLAM-Datenrate Max.']} / {vals['DSLAM-Datenrate Max.']} kbit/s (Cable: {vals['DSLAM-Datenrate Max.']} kbit/s)")
+            publish_stats(vals)
+        except Exception as e: log(f"Error: {e}")
+        await sleep_s(10)
+
+if __name__ == "__main__":
+    if (len(argv) > 1): sid = argv[1]
+    run(main())
