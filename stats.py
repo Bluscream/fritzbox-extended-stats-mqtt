73f41d9fc96aa96c99264d1c2c3f4ccce099a15e
Bluscream <Bluscream@users.noreply.github.com>
2024-02-01 11:51:47 +0100

    move to ha-mqtt-discoverable

--- stats.py
+++ stats.py
@@ -7,9 +7,11 @@ from hashlib import md5
 from sys import argv
 from ssl import _create_unverified_context
 from os import getenv
+from typing import Any, Optional
+from sys import maxsize
 
-
-import paho.mqtt.client as mqtt
+from ha_mqtt_discoverable import Settings
+from ha_mqtt_discoverable.sensors import Sensor, SensorInfo, DeviceInfo, NumberInfo, TextInfo, Number, Text
 from http.client import HTTPSConnection, HTTPConnection
 from asyncio import sleep as sleep_s # from time import sleep
 from asyncio import run
@@ -20,6 +22,7 @@ from responses.data import Root
 
 
 load_dotenv()
+uid_prefix = getenv("MQTT_UID_PREFIX") or "mqtt_fritzbox_"
 payload = {
     'xhr': '1',
     'lang': 'en',
@@ -104,7 +107,7 @@ sensors = {
         "Latenz"
     ]
 }
-sensors_published = []
+sensors_published = {}
 
 last_log = ''
 def log(msg: str, ts: datetime = None):
@@ -114,6 +117,7 @@ def log(msg: str, ts: datetime = None):
     print(f"{ts or datetime.now()} - {msg}")
 def sanitize(name: str) -> str:
     return name.lower().replace('ä','ae').replace('ü','ue').replace('ö','oe').replace(' ','_').replace('.','').replace('(','').replace(')','') # .join([c for c in name if ord(c) < 128 or c == '_']).lower()
+def is_number(val: object) -> bool: return isinstance(val, int) or isinstance(val, float)
 def dicts_are_different(a: dict, b: dict) -> bool:
     if a is None or b is None: return True
     if a.keys() != b.keys(): return True
@@ -127,55 +131,44 @@ def get_changes_of_dicts(a: dict, b: dict) -> dict:
     for key in a.keys():
         if a[key] != b[key]: changes[key] = a[key]
     return changes
+def parse_dict(d: dict) -> dict:
+    for key, val in d.items():
+        if isinstance(val, dict): d[key] = parse_dict(val)
+        elif isinstance(val, str):
+            if val.isdigit(): d[key] = int(val)
+            elif val.replace('.','',1).isdigit(): d[key] = float(val)
+    return d
 
-mqtt_client = mqtt.Client()
+mqtt_settings = Settings.MQTT(host=getenv("MQTT_IP"), port=int(getenv("MQTT_PORT")), username=getenv("MQTT_USERNAME"), password=getenv("MQTT_PASSWORD"), )
+mqtt_device = DeviceInfo(name="Fritz!Box", identifiers="mqtt.fritzbox", model="Fritz!Box 7590", manufacturer="AVM")
 
-def mqtt_connect():
-    try:
-        mqtt_client.connect(getenv("MQTT_IP"), int(getenv("MQTT_PORT")), 60)
-        mqtt_client.loop_start()
-        mqtt_client.username_pw_set(getenv("MQTT_USERNAME"), getenv("MQTT_PASSWORD"))
-    except Exception as e: log(f"Error connecting to MQTT: {e}")
+def my_callback(client: Any, user_data, message: Any): pass
 
-def setup_mqtt(online: bool = True):
-    discovery_topic = f"homeassistant/sensor/{getenv('MQTT_TOPIC')}"
-    discovery_payload = json_dumps({ "availability": "online" if online else "offline"})
-    log(f"{discovery_topic}: {discovery_payload}")
-    if not mqtt_client.is_connected(): mqtt_connect()
-    mqtt_client.publish(discovery_topic, discovery_payload)
-    mqtt_client.publish(discovery_topic+'/config', discovery_payload)
+def publish_sensor(uid: str, name: str, val: object):
+    info = None
+    is_num = is_number(val)
+    log(f'name: {name} | isnum: {is_num} | type: {type(val)}')
+    template = {}
+    for cat_name, category in sensors.items():
+        if name in category:
+            template = sensor_templates[cat_name]
+            break
+    if is_num: info = NumberInfo(name=name, device=mqtt_device, unique_id=uid, min=maxsize*-1, max=maxsize, mode=None, icon=template.get('icon') or None, state_class=template.get('state_class') or None, unit_of_measurement=template.get('unit_of_measurement') or None, device_class=template.get('device_class') or None)
+    else: info = TextInfo(name=name, device=mqtt_device, unique_id=uid, icon=template.get('icon') or None, state_class=template.get('state_class') or None, unit_of_measurement=template.get('unit_of_measurement') or None, device_class=template.get('device_class') or None)
+    settings = Settings(mqtt=mqtt_settings, entity=info)
+    entity = None
+    if is_num: entity = Number(settings, my_callback)
+    else: entity = Text(settings, my_callback)
+    sensors_published[name] = entity
 def publish_stats(vals: dict[str, str]):
-    topic = getenv("MQTT_TOPIC")
     for name, val in vals.items():
-        uid = sanitize(name)
-        mqtt_client.publish(f'{topic}/{uid}', val)
-        if name in sensors_published: continue
-        for cat_name, category in sensors.items():
-            if name in category:
-                discovery_topic = f"homeassistant/sensor/{topic}/{uid}/config"
-                discovery_payload = sensor_templates[cat_name]
-                discovery_payload['name'] = name
-                discovery_payload['id'] = f'mqtt_fritzbox_{uid}'
-                discovery_payload['state_topic'] = f'{topic}/{uid}'
-                discovery_payload['device'] = {
-                    "identifiers": "mqtt.fritzbox",
-                    "manufacturer": "AVM",
-                    "model": "Fritz!Box 7590",
-                    "name": "Fritz!Box",
-                    "sw_version": "Unknown"
-                },
-                _json = json_dumps(discovery_payload)
-                log(f"{discovery_topic}: {_json}")
-                if not mqtt_client.is_connected(): mqtt_connect()
-                mqtt_client.publish(discovery_topic, _json)
-                sensors_published.append(name)
-                break
+        uid = uid_prefix+sanitize(name)
+        if not name in sensors_published.keys(): publish_sensor(uid, name, val)
+        if isinstance(sensors_published[name], Number): sensors_published[name].set_value(val)
+        elif isinstance(sensors_published[name], Text): sensors_published[name].set_text(val)
 
 
-conn = HTTPSConnection(
-    getenv("FRITZBOX_IP"),
-    context = _create_unverified_context()
-)
+conn = HTTPSConnection(getenv("FRITZBOX_IP"),context = _create_unverified_context())
 def get_fritzbox_sid():
     global payload
     conn.request("GET", "/login_sid.lua")
@@ -186,7 +179,6 @@ def get_fritzbox_sid():
     conn.request("GET", "/login_sid.lua?username={}&response={}".format(getenv("FRITZBOX_USERNAME"), response_string))
     response = conn.getresponse()
     sid = ElementTree.fromstring(response.read()).findall(".//SID")[0].text
-    # conn.close()
     log(f"Got new sid: {sid}")
     payload['sid'] = sid
     return sid
@@ -207,25 +199,22 @@ def get_stats():
 
 async def main():
     last_vals = None
-    setup_mqtt()
     while True:
-        try:
+        # try:
             stats = get_stats()
             vals = stats.data.get_all_values() # .get_values(important_values)
             changed = get_changes_of_dicts(vals, last_vals)
             if len(changed) < 1:
-                log("No change")
+                log("No changes")
                 continue
             last_vals = vals
-            # for name, val in vals.items():
-            #     log(f"{name}: {val}")
-            log(f"DSLAM: {vals['DSLAM-Datenrate Min.']} / {vals['DSLAM-Datenrate Max.']} kbit/s (Cable: {vals['Leitungskapazität']} kbit/s | ~{vals['ungefähre Leitungslänge']}m)")
-            publish_stats(changed)
-        except Exception as e:
-            log(f"Error: {e}")
-            setup_mqtt(False)
-        try: await sleep_s(10)
-        except: sleep(10)
+            log(f"Changes: {changed}")
+            # log(f"DSLAM: {vals['DSLAM-Datenrate Min.']} / {vals['DSLAM-Datenrate Max.']} kbit/s (Cable: {vals['Leitungskapazität']} kbit/s | ~{vals['ungefähre Leitungslänge']}m)")
+            publish_stats(parse_dict(changed))
+        # except Exception as e:
+        #     log(f"Error: {e}")
+            try: await sleep_s(10)
+            except: sleep(10)
 
 if __name__ == "__main__":
     if (len(argv) > 1): sid = argv[1]
